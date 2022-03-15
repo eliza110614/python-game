21db2644c20121197f8876235ec0e47dcaaafa7b
Eliza Oord <101002283+eliza110614@users.noreply.github.com>
2022-03-15 16:40:18 +0000

    added more comments

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 # main loop
 while True:
   
-  # welcome user
+  # welcome user and give the instructions on how to play the game
   name = input("What is your name? ")
   print()
   print("Hello, " + name, "let's play Hangman!")
@@ -15,17 +15,17 @@ while True:
   words = ["informatica", "informatiekunde", "spelletje", "aardigheidje", "scholier", "fotografie", "waardebepaling", "specialiteit", "verzekering", "universiteit", 'heesterperk']
 
   # hangman graphics that will be displayed
-  hangman_graphics = ['__\n |\n',
-                   '__\n |\n O',
-                   '__\n |\n O\n/|',
-                   '__\n |\n O\n/|\ ',
-                   '__\n |\n O\n/|\ \n/ \ '
-                   ]
+  hangman_graphics = ["__\n |\n O",
+                     "__\n |\n O\n/|",
+                     "__\n |\n O\n/|\ ",
+                     "__\n |\n O\n/|\ \n/",
+                     "__\n |\n O\n/|\ \n/ \ "
+                    ]
 
   # legenda
   number_mistakes = 0
   letters_guessed = []
-  number_mistakes_allowed = len(hangman_graphics)
+  number_mistakes_allowed = len(hangman_graphics) # = 5 guesses
   word = random.choice(words)
   letters_word = list(word)
   wrong_letters = []
@@ -33,21 +33,26 @@ while True:
   # number of letters in the word
   print()
   print("The word has {} letters".format(len(letters_word)))
-  
+
+  # the continuation of the game while you still have guesses left
+  # printing the letters the player has guessed wrongly 
   while number_mistakes < number_mistakes_allowed:
     print()
     print("Wrong letters: ", end= "")
     for letter in wrong_letters:
       print("{}, ".format(letter), end= "")
     print()
-    # less guesses because user entered a wrong character
+    # less guesses because the player entered a wrong character
     print("Guesses left: {}".format(number_mistakes_allowed - number_mistakes))
     guess = input("Enter a letter: ")
+   
     while guess in letters_guessed or guess in wrong_letters:
       print()
       # multiple entered letters
       print("You have already entered this character, try again")
       guess = input("Enter a letter: ")
+      
+    # not being able to enter more than 1 character nor a number 
     if len(guess) != 1 or not guess.isalpha():
       print()
       print("Invalid input. Try a letter of the alphabet")
@@ -82,9 +87,11 @@ while True:
         print(letter + " ", end= "")
       # showing incorrectly guessed letters as dashes
       else:
-        dash = print("_ ", end= "")
+        print("_ ", end= "")
         b = 2
-        
+
+    # end of the game --> win
+    # player has won if there are no dashes in the printed word anymore: this is done with the global b = 1 and b = 2
     if b == 2:
       continue
     elif b == 1:
@@ -93,17 +100,20 @@ while True:
       break
 
     # able to keep entering letters if you haven't made the maximum amount of           mistakes
-    if number_mistakes < number_mistakes_allowed and b != 1:
+    if number_mistakes < number_mistakes_allowed:
       guess = input("Enter a letter = ")
       
-  # end of the game -> win or loose
+  # end of the game --> loose
+  # player has lost if the amount of mistakes he made is equal to the amount of mistakes that are allowed
   if number_mistakes == number_mistakes_allowed:
     print()
-    print("__\n |\n* *\n/|\ \n/ \ ")
+    # printing a dead hangman
+    print("__\n |\n * \n/|\ \n/ \ ")
     print("You have been hanged :(")
+    # showing the correct word to the player
     print("The correct word is:", word)
     
-  # re-executing game when first round ends
+  # re-executing game when round ends
   endgame = input("Do you want to play again? Enter y if yes, n if no: ").lower()
   if endgame == "n":
     print()
